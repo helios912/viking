36e7123f976b803fcbfe031aa871116db29c6bec
Mukola2 <janaon333@gmail.com>
2024-04-08 16:06:36 +0300

    adaptiv

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-$(function () {});
-Fancybox.bind("[data-fancybox]", {
+$(function () {
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="images/arrowprev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="images/arrownext.svg" alt=""></button>',
+    asNavFor: '.heroes__slider-text',
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    asNavFor: '.heroes__slider-img',
+  });
+});
+Fancybox.bind('[data-fancybox]', {
   // Custom options
 });
