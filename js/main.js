a3835eec676c207cbb9f18a1032babd11ecbf408
Mukola2 <janaon333@gmail.com>
2024-04-09 17:18:46 +0300

    adaptful

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+  $('.menu__btn').on('click', function () {
+    $('.header__menu').toggleClass('header__menu--active');
+    $('.menu__btn').toggleClass('header__btn--active');
+  });
   $('.heroes__slider-img').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -7,6 +11,18 @@ $(function () {
     nextArrow:
       '<button type="button" class="slick-btn slick-next"><img src="images/arrownext.svg" alt=""></button>',
     asNavFor: '.heroes__slider-text',
+    responsive: [
+      {
+        breakpoint: 840,
+        settings: {
+          arrows: false,
+        },
+      },
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
   $('.heroes__slider-text').slick({
     slidesToShow: 1,
